30af5ddfd15d3da76915bdff8cfcd622e1c5f714
geeta-1407 <geetaanjali.codenote@gmail.com>
2023-09-13 17:05:53 +0530

    sef

---
+++ main.js
@@ -0,0 +1,13 @@
+/*Sticy Navigation*/
+
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+  // get the complete hight of window
+  let oTop = $(".section-2").offset().top - window.innerHeight;
+  if ($(window).scrollTop() > oTop) {
+    navbar.addClass("sticky");
+  } else {
+    navbar.removeClass("sticky");
+  }
+});
